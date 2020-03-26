6d81b6cae98405b24ae30caf1138a71c7434a83d
Johanna Roll <johanna-roll@outlook.de>
2020-03-26 16:57:54 +0100

    forschleife über ein array

--- javascript/hello.js
+++ javascript/hello.js
@@ -78,4 +78,7 @@ for (let i=0; i<10; i++) {
     console.log(`Schleife ${i}`);
 }
 
-
+for (let j = 0; j < participants.length; j++) {
+    const participant = participants[j];
+    console.log(`Teilnehmer*in ${j}: ${participant}`);
+}
