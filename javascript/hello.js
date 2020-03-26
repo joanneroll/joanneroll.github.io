eff0b0fa27611410538ff5c5652e08569c2651ff
Johanna Roll <johanna-roll@outlook.de>
2020-03-26 16:05:52 +0100

    string beispiele

--- javascript/hello.js
+++ javascript/hello.js
@@ -1,1 +1,22 @@
 // alert("Hello worlds");
+
+// let message = "Hello world";
+// alert(message);
+// message = "Hallo Welt";
+// alert(message)
+
+const LINK_COLOR = "#ff0000"
+console.log("Link bitte in der Farbe", LINK_COLOR);
+
+let highscore = 238582;
+console.log(highscore / 10);
+
+let firstname = "John";
+let lastname = "Smith";
+console.log("Name: ", firstname, lastname);
+
+let fullname = 'Jeffrey "The Dude" Lebobski';
+console.log(fullname);
+
+let template = `Dein Highscore sind ${highscore} Punkte`;
+console.log(template)
