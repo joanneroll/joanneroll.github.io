60c45387361f7ae7499807247f346ef6ca55ecd0
Johanna Roll <johanna-roll@outlook.de>
2020-03-26 16:33:18 +0100

    erste if schleife

--- javascript/hello.js
+++ javascript/hello.js
@@ -59,3 +59,14 @@ console.log(a+b);
 console.log(b/(a-1));
 a++; //+1
 console.log(a);
+
+
+// let myAge = prompt("Wie alt bist du?");
+// console.log(`Du bist ${myAge} Jahre alt.`)
+// console.log(`über 18? ${myAge > 18}`);
+
+// if (myAge >18) {
+//     console.log("Glückwunsch: über 18");
+// } else {
+//     console.log("Leider unter 18.")
+// }
