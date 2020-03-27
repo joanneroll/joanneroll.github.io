6bdfdac9538c56b9bdfc3f5b3e0ccc8d79d95388
Johanna Roll <johanna-roll@outlook.de>
2020-03-27 11:33:16 +0100

    Add Map of Milford Sound with TopoBaseLayer

--- nz/main.js
+++ nz/main.js
@@ -1,15 +1,24 @@
 // Leaflet 
 let Map = document.querySelector("#map");
+let lat = Map.dataset.lat;
+let lng = Map.dataset.lng;
 
-var mymap = L.map(Map).setView([51.505, -0.09], 13);
+// console.log(lat, lng);
 
-L.tileLayer(
-    'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-        maxZoom: 18,
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-            '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-            'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        id: 'mapbox/streets-v11',
-        tileSize: 512,
-        zoomOffset: -1
-    }).addTo(mymap);
+var mymap = L.map(Map).setView([lat, lng], 13);
+
+// L.tileLayer(
+//     'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+//         maxZoom: 18,
+//         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+//             '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+//             'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//         id: 'mapbox/streets-v11',
+//         tileSize: 512,
+//         zoomOffset: -1
+//     }).addTo(mymap);
+
+L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    maxZoom: 17,
+    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+}).addTo(mymap);
