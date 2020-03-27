3b43d14b9c77bbc51527de406d04832df7a916e2
Johanna Roll <johanna-roll@outlook.de>
2020-03-27 11:46:36 +0100

    Reorder Java Script, Include Satelliteview

--- nz/main.js
+++ nz/main.js
@@ -7,6 +7,14 @@ let lng = Map.dataset.lng;
 
 var mymap = L.map(Map).setView([lat, lng], 13);
 
+// Topographische Karte
+L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    maxZoom: 17,
+    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
+}).addTo(mymap);
+
+
+// // Alternativer Openstreetlayer (streets/satellite)
 // L.tileLayer(
 //     'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
 //         maxZoom: 18,
@@ -14,11 +22,7 @@ var mymap = L.map(Map).setView([lat, lng], 13);
 //             '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
 //             'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
 //         id: 'mapbox/streets-v11',
+//         //id: 'mapbox/satellite-v9',
 //         tileSize: 512,
 //         zoomOffset: -1
 //     }).addTo(mymap);
-
-L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-    maxZoom: 17,
-    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-}).addTo(mymap);
