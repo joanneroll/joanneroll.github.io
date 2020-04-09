e6b1b623be221a21527781ff0f8fbc947288ac64
Johanna Roll <johanna-roll@outlook.de>
2020-04-09 15:46:32 +0200

    Header Index

--- world/main.js
+++ world/main.js
@@ -23,6 +23,9 @@ L.control.layers({
 
 let drawCircles = function () {
     let data = CONFIRMED;
+    let header = CONFIRMED[0];
+    let index = header.length -1; //wir wollen den letzten Dateneintrag erwischen
+
     //console.log(data);
     for (let i = 1; i < data.length; i++) {
         let row = data[i];
@@ -30,7 +33,7 @@ let drawCircles = function () {
         let reg = `${row[0]} ${row[1]}`;
         let lat = row[2];
         let lng = row[3];
-        let val = row[row.length - 1];
+        let val = row[index];
         //let mrk = L.marker([lat,lng]).addTo(map);
         //mrk.bindPopup(`${reg}: ${val}`);
 
