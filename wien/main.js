1f24c4200880834754ad9f1923a86b789df30fad
Johanna Roll <johanna-roll@outlook.de>
2020-04-16 17:11:44 +0200

    Hinzufügen der Wandern und Heritage Datensätze

--- wien/main.js
+++ wien/main.js
@@ -38,7 +38,7 @@ let walk = L.geoJson.ajax(walkUrl, { //Punkte werden automatisch als Marker gese
         let marker = L.marker(latlng, {
             icon: icon
         });
-        console.log("Point", point);
+        // console.log("Point", point);
         marker.bindPopup(`<h3>${point.properties.NAME}</h3>
         <p><a target="links" href="${point.properties.WEITERE_INF}">Link</a></p>`); //bei nur "points" würde {object Object} kommen
         return marker;
@@ -50,3 +50,24 @@ walk.on("data:loaded", function() { //wenn das Event walk geladen wurde (asynchr
     console.log("data loaded");
     map.fitBounds(walkGroup.getBounds()); //Kartengrenzen an walkGroup Gruppe ausrichten
 })
+
+let wandern = "https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:WANDERWEGEOGD&srsName=EPSG:4326&outputFormat=json";
+
+L.geoJson.ajax(wandern, {
+    style: function () {
+        return { color: "green", weight: 5}
+    }
+}).addTo(map);
+
+let heritage = "https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:WELTKULTERBEOGD&srsName=EPSG:4326&outputFormat=json";
+
+L.geoJson.ajax(heritage, {
+    style: function() {
+        return { color: "salmon", fillOpacity: 0.3};
+    }, 
+    onEachFeature: function (feature, layer) {
+        console.log("Feature: ", feature);
+        layer.bindPopup(`<h3>${feature.properties.NAME}</h3>
+        <p>${feature.properties.INFO}</p>`)
+    }
+}).addTo(map);
