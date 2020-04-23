f74566c31da0353e50b54c68ca1f92bd3b199db7
Johanna Roll <johanna-roll@outlook.de>
2020-04-23 18:16:32 +0200

    Rotation der Windpfeile in Windrichtung

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -118,12 +118,13 @@ let drawWind = function (jsonData) {
         pointToLayer: function (feature, latlng) {
 
             let windKMH = Math.round(feature.properties.WG * 3.6);
-            color = getColor(windKMH, COLORS.wind);
+            let color = getColor(windKMH, COLORS.wind);
+            let rotation = feature.properties.WR;
 
             return L.marker(latlng, {
                 title: `${feature.properties.name} (${feature.geometry.coordinates[2]}m) - ${windKMH} km/h`,
                 icon: L.divIcon({
-                    html: `<div class="label-wind"><i class="fas fa-arrow-circle-up" style="color:${color}"></i></div>`,
+                    html: `<div class="label-wind"><i class="fas fa-arrow-circle-up" style="color:${color};transform: rotate(${rotation}deg)"></i></div>`,
                     className: "ignore-me"
                 })
 
