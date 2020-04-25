95f3933fe43bd787709598e92dc83d64428a8ccf
Johanna Roll <johanna-roll@outlook.de>
2020-04-25 12:02:12 +0200

    Cleanen und Tunen der Kommentare

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -36,15 +36,6 @@ let awsUrl = "https://aws.openweb.cc/stations";
 let aws = L.geoJson.ajax(awsUrl, {
     filter: function (feature) {
         // console.log("Feature in filer: ", feature);
-        // console.log(feature.properties.LT)
-        // if (feature.properties.LT < 5) {
-        //     return true;
-        // } else {
-        //     return false;
-        // }
-        // return feature.properties.LT <5;
-        // return feature.geometry.coordinates[2] > 3000,
-        // return feature.properties.LT != null;
         return feature.properties.LT;
     },
     pointToLayer: function (point, latlng) {
@@ -53,9 +44,8 @@ let aws = L.geoJson.ajax(awsUrl, {
             `<ul>` +
             `<li><b>Position:</b> Lat: ${point.geometry.coordinates[0].toFixed(5)}/Lng: ${point.geometry.coordinates[1].toFixed(5)}</li>` +
             `<li><b>Datum:</b> ${point.properties.date}</li>` +
-            `<li><b>Temperatur:</b> ${point.properties.LT} °C</li>`
+            `<li><b>Temperatur:</b> ${point.properties.LT} °C</li>` +
             //sind folgende Parameter für die Station nicht definiert, erscheinen sie auch nicht im Popup
-            +
             (typeof point.properties.WG !== "undefined" ? `<li><b>Windgeschwindigkeit:</b> ${point.properties.WG} m/s</li>` : "") +
             (typeof point.properties.RH !== "undefined" ? `<li><b>Relative Luftfeuchte:</b> ${point.properties.RH} %</li>` : "") +
             (typeof point.properties.HS !== "undefined" ? `<li><b>Schneehöhe:</b> ${point.properties.HS} cm</li>` : "") +
@@ -67,8 +57,14 @@ let aws = L.geoJson.ajax(awsUrl, {
     }
 }).addTo(overlay.stations);
 
+//ajax geoJSON ->  in der Variable "aws" werden die Daten vom Server abgerufen
+//möchte man die Daten in individuell als Layer darstellen, anders visualisieren, ... müssen entsprechende ZeichenFunktionen erstellt werden
+//diese können aufgerufen werden, sobald die Daten geladen wurden ("aws.on")
+//dabei muss das geoJSON Objekt aus der "aws" Funktion wieder abgerufen werden und den ZeichenFunktionen übergeben werden
+
+//Farbzuweisung anhang der Breaks
 let getColor = function (val, ramp) {
-    console.log(val, ramp);
+    // console.log(val, ramp);
     //Solange der Wert niedriger ist als die Schwelle, Farbcode übergeben
     for (let i = 0; i < ramp.length; i++) {
         const pair = ramp[i];
@@ -84,22 +80,21 @@ let getColor = function (val, ramp) {
 
 };
 
-//console.log(color);
-
+//ZeichenFunktionen der einzelnen Parameter/DatenLayer
 let drawTemperature = function (jsonData) {
-    console.log(jsonData);
+    // console.log(jsonData);
     L.geoJson(jsonData, {
         filter: function (feature) {
             return feature.properties.LT
         },
         pointToLayer: function (feature, latlng) {
             color = getColor(feature.properties.LT, COLORS.temperature);
-            console.log(color);
+            // console.log(color);
             return L.marker(latlng, {
                 title: `${feature.properties.name} (${feature.geometry.coordinates[2]}m)`,
                 icon: L.divIcon({
                     html: `<div class="label-temperature" style="background-color:${color}">${feature.properties.LT.toFixed(1)}</div>`,
-                    className: "ignore-me" // dirty hack - Standard StyleClass wird überschrieben
+                    className: "ignore-me" // dirty hack - Standard StyleClass wird überschrieben und im CSS neu definiert
                 })
 
             });
@@ -134,17 +129,14 @@ let drawWind = function (jsonData) {
 
 };
 
-
-
+//Aufrufen der ZeichenFunktionen
 aws.on("data:loaded", function () {
     // console.log(aws.toGeoJSON()); //aws wieder als GeoJSON abrufen
-
-    drawTemperature(aws.toGeoJSON()); //Funktion wird aufgerufen und GeoJSON Objekt übergeben
+    //Funktion wird aufgerufen und GeoJSON Objekt übergeben
+    drawTemperature(aws.toGeoJSON());
     drawWind(aws.toGeoJSON());
 
     map.fitBounds(overlay.stations.getBounds()); //Boundaries auf angezeigte Station setzen 
 
-    overlay.wind.addTo(map); //dieser Layer wird beim Start angezeigt
-
-    // console.log(COLORS);
+    overlay.wind.addTo(map); //dieser Layer wird als default angezeigt
 });
