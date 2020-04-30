56031ddc75843551d954b7409e25ecb2a231361c
Johanna Roll <johanna-roll@outlook.de>
2020-04-30 16:41:10 +0200

    adlerblicke overlay

--- adlerweg/main.js
+++ adlerweg/main.js
@@ -8,6 +8,10 @@ let map = L.map("map", {
     ]
 });
 
+let overlay = {
+    adlerblicke: L.featureGroup()
+};
+
 L.control.layers({
     "BasemapAT.grau": L.tileLayer.provider("BasemapAT.grau"),
     "BasemapAT": L.tileLayer.provider("BasemapAT"),
@@ -20,6 +24,8 @@ L.control.layers({
         L.tileLayer.provider("BasemapAT.orthofoto"),
         L.tileLayer.provider("BasemapAT.overlay")
     ])
+}, {
+    "Adlerblicke": overlay.adlerblicke
 }).addTo(map);
 
 
@@ -30,4 +36,8 @@ L.control.layers({
 
 for (const blick of ADLERBLICKE) {
     console.log(blick);
-}
+    let mrk = L.marker ([blick.lat,blick.lng]).addTo(overlay.adlerblicke);
+    mrk.bindPopup (`Standort ${blick.standort} (${blick.seehoehe}m)`);
+    }
+
+overlay.adlerblicke.addTo(map);
