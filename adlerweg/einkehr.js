edfd85c65459dfae4aadfa0739ed10b149aecb59
Johanna Roll <johanna-roll@outlook.de>
2020-05-06 12:41:08 +0200

    einkehr.js - Etappen 5-8

---
+++ adlerweg/einkehr.js
@@ -0,0 +1,17 @@
+const EINKEHR = [
+    [5, Buchacker Alm, 47.5332995, 12.0177095],
+    [5, Gasthaus Gwercherwirt, 47.5187834, 11.8971923],
+    [6, Gasthaus Gwercherwirt, 47.5187834, 11.8971923],
+    [6, Gasthof Waldhäusl, 47.521603, 11.78475]
+    [7, Gasthof Waldhäusl, 47.521603, 11.78475],
+    [7, Erfurter Hütte, 47.441667, 11.763056],
+    [7, Berggasthof Rofan, 47.44116, 11.7621632],
+    [7, Mauritzalm, 47.4437886, 11.7641593],
+    [8, Erfurter Hütte, 47.441667, 11.763056],
+    [8, Berggasthof Rofan, 47.441218, 11.763724],
+    [8, Mauritzalm, 47.4437886, 11.7641593],
+    [8, Falzthurnalm, 47.4261142, 11.6381214],
+    [8, Sennhütte, 47.4432844, 11.6382073],
+    [8, Gramaialm, 47.4024584, 11.6070636],
+    [8, Lamsenjochhütte, 47.3800824, 11.6013247]
+];
