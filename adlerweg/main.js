0bf1ac7eaf189ef282663e44188779e79e8edf2c
Bernd Oeggl <bernd@komplett.cc>
2020-05-07 17:00:04 +0200

    fix: possible fix for profile not at 100% width

--- adlerweg/main.js
+++ adlerweg/main.js
@@ -76,6 +76,7 @@ let drawEtappe = function (nr) {
         map.fitBounds(overlay.adlerblicke.getBounds()); //Mapbounds auf Adlerblicke
         // visibility = document.getElementsByClassName("optional-visible").style = "optional-hidden";
         vis = document.getElementsByClassName("optional");
+        document.getElementById("profile").style.visibility = "hidden";
         for (let i = 0; i < vis.length; i++) {
             vis[i].style.display = "none";
         }
@@ -93,6 +94,7 @@ let drawEtappe = function (nr) {
 
     } else {
         // visibility = document.getElementsByClassName("optional-hidden").className = "optional-visible";
+        document.getElementById("profile").style.visibility = "visible";
         vis = document.getElementsByClassName("optional");
         for (let i = 0; i < vis.length; i++) {
             vis[i].style.display = "inline";
